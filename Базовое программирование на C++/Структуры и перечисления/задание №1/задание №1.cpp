3991b148bd5e5a7f1027032a60f109c77f221509
MrCatVolt <mrcatvolt@gmail.com>
2023-07-05 00:28:26 +0300

    2урок задание 1

---
+++ Базовое программирование на C++/Структуры и перечисления/задание №1/задание №1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+
+enum month {
+    досвидание,
+    январь,
+    февраль,
+    март,
+    апрель,
+    май,
+    июнь,
+    июль,
+    август,
+    сентябрь,
+    октябрь,
+    ноябрь,
+    декабрь
+};
+
+
+int main()
+{
+    int a = 0, b = 0;
+    std::cout << "Введите номер месяца:\n";
+    std::cin >> a;
+    month z = static_cast<month>(январь);
+    //seasons season = static_cast<seasons>(1);
+    std::cout << z << std::endl;
+
+
+}
+
+

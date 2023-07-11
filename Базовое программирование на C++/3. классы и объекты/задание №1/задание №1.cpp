851c71c9ef275c49daedc9baee78200145b87b57
MrCatVolt <mrcatvolt@gmail.com>
2023-07-11 15:04:15 +0300

    классы и объекты

---
+++ Базовое программирование на C++/3. классы и объекты/задание №1/задание №1.cpp
@@ -0,0 +1,105 @@
+﻿#include <iostream>
+
+class Calculator {
+public:
+    double num1;
+    double num2;
+
+    double add(double num1, double num2) {
+        double x=0;
+        x = num1 + num2;
+        std::cout << "num1 + num2 = " << x << "\n";
+        return x;
+    }
+
+    double multiply (double num1, double num2) {
+        double x = 0;
+        x = num1 * num2;
+        std::cout << "num1 * num2 = " << x << "\n";
+        return x;
+    }
+
+    double subtract_1_2(double num1, double num2) {
+        double x = 0;
+        x = num1 - num2;
+        std::cout << "num1 - num2 = " << x << "\n";
+        return x;
+    }
+
+    double subtract_2_1(double num1, double num2) {
+        double x = 0;
+        x = num2 - num1;
+        std::cout << "num2 - num1 = " << x << "\n";
+        return x;
+    }
+
+    double divide_1_2(double num1, double num2) {
+        double x = 0;
+        x = num1 / num2;
+        std::cout << "num1 / num2 = " << x << "\n";
+        return x;
+    }
+
+    double divide_2_1(double num1, double num2) {
+        double x = 0;
+        x = num2 / num1;
+        std::cout << "num2 / num1 = " << x << "\n";
+        return x;
+    }
+
+    bool set_num1(double num1) {
+        if (num1 == 0) {
+            return false;
+        }
+        else {
+            this->num1 = num1;
+            return true;
+        }
+    }
+
+    bool set_num2(double num2) {
+        if (num2 == 0) {
+            return false;
+        }
+        else {
+            this->num2 = num2;
+
+            return true;
+        }
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double a = 0, b = 0;
+    int i = 5;
+    while (i == 5) {
+                
+        std::cout << "\n\nвведите num1:\n";
+        std::cin >> a;
+
+        std::cout << "введите num2:\n";
+        std::cin >> b;
+
+        Calculator Calculator;
+        Calculator.set_num1(a);
+        Calculator.set_num2(b);
+        
+
+
+        if (Calculator.set_num1(a) == 0 || Calculator.set_num2(b) == 0) {
+            std::cout << "Неверный ввод!\n";
+        }
+        else {
+            Calculator.add(a, b);
+            Calculator.multiply(a, b);
+            Calculator.subtract_1_2(a, b);
+            Calculator.subtract_2_1(a, b);
+            Calculator.divide_1_2(a, b);
+            Calculator.divide_2_1(a, b);
+        }
+    }
+}
+
+
---
+++ Базовое программирование на C++/3. классы и объекты/задание №2/задание №2.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <string>
+#include <cstring>
+#include <Windows.h>
+
+class Counter {
+private:
+    int x;
+
+public:
+    Counter (){
+        x = 1;
+    }
+
+    Counter (int x) {
+        this->x = x;
+    }
+
+    void set_x_on1() {
+        x++;
+    }
+
+    void set_x_off1() {
+        --x;
+    }
+
+    int get_x_cout() {
+        std::cout << "\n" << x << "\n";
+        return x;
+    }
+
+};
+
+
+
+
+
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int a = 0;
+    std::string b;
+    char c;
+    
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет:\n";
+    std::cin >> b;
+
+    if (b == "да") {
+        std::cout << "Введите начальное значение счётчика:\n";
+        std::cin >> a;
+        Counter z(a);
+    }
+    else {
+        Counter z();
+    }
+
+
+    int i = 0;
+    while (i==0) {
+        std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        std::cin >> c;
+        char d = '+', d2 = '-', d3 = '=', d4 = 'x';
+
+
+        if (c == d) {
+            z.set_x_on1();
+        }
+        if (c == d2) {
+            z.set_x_off1();
+        }
+        if (c == d3) {
+            z.get_x_cout();
+            std::cout << "\n" << z.get_x_cout() << "\n";
+        }
+        if (c == d4) {
+            std::cout << "До свидания!";
+            break;
+        }
+    }
+}
+

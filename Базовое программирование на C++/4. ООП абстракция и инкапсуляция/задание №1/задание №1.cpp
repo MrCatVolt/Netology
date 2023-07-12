67a8c487623ba0da2dc5f4540ac278d830d28bdd
MrCatVoltSamsung <mrcatvolt@gmail.com>
2023-07-12 22:39:33 +0300

    ооп задание 1 переделка

--- Базовое программирование на C++/4. ООП абстракция и инкапсуляция/задание №1/задание №1.cpp
+++ Базовое программирование на C++/4. ООП абстракция и инкапсуляция/задание №1/задание №1.cpp
@@ -30,12 +30,30 @@ public:
 
     int converting(std::string x) {
         int x2 = 0;
-        x2 = static_cast<int>(x);
+        x2 = std::stoi(x);
+        //x2 = static_cast<int>(x);
         return x2;
     }
 
+    std::string get_sity() {
+        std::string z = sity;
+        return z;
+    }
 
+    std::string get_street() {
+        std::string z = street;
+        return z;
+    }
 
+    int get_num_home() {
+        int z = num_home;
+        return z;
+    }
+
+    int get_num_apartments() {
+        int z = num_apartments;
+        return z;
+    }
 };
 
 
@@ -44,40 +62,37 @@ int main()
     setlocale (LC_ALL, "Russian");//65001 для UTF-8, 1251 для виндовс Идентификаторы кодовой страницы
     SetConsoleCP (1251);
     SetConsoleOutputCP (1251);
-    std::fstream book("in.txt");
+    std::ifstream book("in.txt"); // открываем файл на запись
     int a = 0;
-
-    //Counter* z;
-    //(b == "да") ? (z = new Counter(a)) : (z = new Counter()
-    
     book >> a;
-
+    
     Addresses* A = new Addresses [a];
 
-
-
-
-    for (int i = 0; i < a; i++) {
+    for (int i = 0; i < a; i++) { // считываем файл в экземпляр класса
         std::string b1, b2, b3, b4;
+        int b5=0, b6=0;
         book >> b1 >> b2 >> b3 >> b4;
 
+        b5= A[i].converting (b3);
+        b6= A[i].converting(b4);
 
-
-
-
-        A[i].set_init(b1, b2, b3, b4);
-    }
-
-
-
-     
-
+       A[i].set_init(b1, b2, b5, b6);
     }
 
 
 
 
+    std::ofstream book_off("out.txt"); //работаем на передачу в файл
+
+    for (int i = a-1; i > 1; i--) {
+        book_off << A[i].get_sity() << A[i].get_street() << A[i].get_num_home() << A[i].get_num_apartments();
+        std::cout << A[i].get_sity() << "  " << A[i].get_street() << "  " << A[i].get_num_home() << "  " << A[i].get_num_apartments();
+
+    }
 
 
-}
-
+    delete A;
+    book.close();
+    book_off.close();
+    
+    }

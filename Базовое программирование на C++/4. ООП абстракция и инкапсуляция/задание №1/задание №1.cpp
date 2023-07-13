bb4936141dc616257048f17c7fecc1656aa06c0b
MrCatVoltSamsung <mrcatvolt@gmail.com>
2023-07-13 23:54:24 +0300

    ООП_1 - задание 1 готово, задание 2 готово

--- Базовое программирование на C++/4. ООП абстракция и инкапсуляция/задание №1/задание №1.cpp
+++ Базовое программирование на C++/4. ООП абстракция и инкапсуляция/задание №1/задание №1.cpp
@@ -13,18 +13,19 @@ private:
 
 public:
 
-    Addresses(std::string sity, std::string street, int num_home, int num_apartments) {
+    /*Addresses(std::string sity, std::string street, int num_home, int num_apartments) {
     this->sity = sity;
     this->street = street;
     this->num_home = num_home;
     this->num_apartments = num_apartments;
     }
+    */
 
     void set_init(std::string sity, std::string street, int num_home, int num_apartments) {
         this->sity = sity;
         this->street = street;
         this->num_home = num_home;
-        this->num_apartments = num_apartments;    
+        this->num_apartments = num_apartments;
     }
 
     int converting(std::string x) {
@@ -58,37 +59,40 @@ public:
 
 int main()
 {
-    setlocale (LC_ALL, "Russian");//65001 для UTF-8, 1251 для виндовс Идентификаторы кодовой страницы
-    SetConsoleCP (1251);
-    SetConsoleOutputCP (1251);
+    setlocale(LC_ALL, "Russian");//65001 для UTF-8, 1251 для виндовс Идентификаторы кодовой страницы
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
     std::ifstream book("in.txt"); // открываем файл на запись
     int a = 0;
     book >> a;
-    
-    Addresses** A = new Addresses* [a];
 
-    for (int i = 0; i < a-1; i++) { // считываем файл в экземпляр класса
+    Addresses* A = new Addresses[a];
+
+    for (int i = 0; i < a; i++) { // считываем файл в экземпляр класса
         std::string b1, b2, b3, b4;
-        int b5=0, b6=0;
+        int b5 = 0, b6 = 0;
         book >> b1 >> b2 >> b3 >> b4;
 
-        b5= A[i][i].converting(b3);
-        b6= A[i][i].converting(b4);
+        b5 = A[i].converting(b3);
+        b6 = A[i].converting(b4);
 
-        A[i] = new Addresses (b1, b2, b5, b6);
-       //A[i].set_init(b1, b2, b5, b6);
+        A[i].set_init(b1, b2, b5, b6);
     }
+
+
 
 
     std::ofstream book_off("out.txt"); //работаем на передачу в файл
     book_off << a << "\n";
-    for (int i = a; i > 1; i--) {
-        book_off << A[i][i].get_sity() << ", " << A[i][i].get_street() << ", " << A[i][i].get_num_home() << ", "
-            << A[i][i].get_num_apartments() << "\n";
-        std::cout << A[i][i].get_sity() << ", " << A[i][i].get_street() << ", " << A[i][i].get_num_home() << ", " << A[i][i].get_num_apartments() << "\n";
+    for (int i = a-1; i > -1; i--) {
+        book_off << A[i].get_sity() << ", " << A[i].get_street() << ", " << A[i].get_num_home() << ", " << A[i].get_num_apartments() << "\n";
+        std::cout << A[i].get_sity() << ", " << A[i].get_street() << ", " << A[i].get_num_home() << ", " << A[i].get_num_apartments() << "\n";
+    
     }
+
 
     delete []A;
     book.close();
     book_off.close();
-    }
+
+}

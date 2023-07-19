0bde6c22d1e7ecc16847d89496279038b68e900a
MrCatVoltSamsung <mrcatvolt@gmail.com>
2023-07-19 23:22:10 +0300

    10_CMake задания 1,2

---
+++ Базовое программирование на C++/10. CMake/1_my_first_cmake_program/1_my_first_cmake_program.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+
+    std::string name;
+    std::cout << "Введите имя:\n";
+    std::cin >> name;
+    std::cout << "\nЗдравствуйте, " << name << "!\n";
+
+    system("pause");
+}
+

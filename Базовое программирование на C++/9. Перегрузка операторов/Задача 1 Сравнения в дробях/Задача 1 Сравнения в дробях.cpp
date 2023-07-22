153050cd4d8faaac7fea865fda52de9577118187
MrCatVoltSamsung <mrcatvolt@gmail.com>
2023-07-22 07:36:16 +0300

    темы 9. перегрузка

--- Базовое программирование на C++/9. Перегрузка операторов/Задача 1 Сравнения в дробях/Задача 1 Сравнения в дробях.cpp
+++ Базовое программирование на C++/9. Перегрузка операторов/Задача 1 Сравнения в дробях/Задача 1 Сравнения в дробях.cpp
@@ -12,12 +12,65 @@ public:
 		numerator_ = numerator;
 		denominator_ = denominator;
 	}
+
+
+	bool operator==(Fraction other) {
+		if ((numerator_ / denominator_) == (other.numerator_ / other.denominator_)) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+
+	bool operator!=(Fraction other){
+		if ((numerator_ / denominator_) != (other.numerator_ / other.denominator_)) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+	bool operator>(Fraction other) {
+		if ((numerator_ / denominator_) > (other.numerator_ / other.denominator_)) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+	bool operator<(Fraction other) {
+		if ((numerator_ / denominator_) < (other.numerator_ / other.denominator_)) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+	bool operator>=(Fraction other) {
+		if ((numerator_ / denominator_) >= (other.numerator_ / other.denominator_)) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+	bool operator<=(Fraction other) {
+		if ((numerator_ / denominator_) <= (other.numerator_ / other.denominator_)) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+
 };
+
 
 int main()
 {
-	Fraction f1(4, 3);
-	Fraction f2(6, 11);
+	Fraction f1(6, 3);
+	Fraction f2(10, 2);
 
 	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
 	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
---
+++ Базовое программирование на C++/9. Перегрузка операторов/задание 2 дроби2/задание 2 дроби2.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+
+class Fraction
+{
+private:
+	int numerator_;
+	int denominator_;
+	double ant_x;
+public:
+	Fraction(int numerator, int denominator)
+	{
+		numerator_ = numerator;
+		denominator_ = denominator;
+	}
+
+	double get_answer() {
+		return ant_x;
+	}
+
+
+	Fraction operator+(Fraction other) {
+		numerator_ = (numerator_ * other.denominator_) + (denominator_ * other.numerator_);
+		denominator_ = (denominator_ * other.denominator_);
+		
+		return Fraction(numerator_, denominator_);
+	}
+
+
+	void get_print() {
+
+		std::cout << numerator_ << "/" << denominator_ << "\n";
+	}
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
+int main()
+{
+	int x1 = 0, x2 = 0, x3 = 0, x4 = 0;
+
+	std::cout << "Введите числитель дроби 1: ";
+	std::cin >> x1;
+
+	std::cout << "\nВведите числитель дроби 2: ";
+	std::cin >> x2;
+
+	std::cout << "\nВведите числитель дроби 3: ";
+	std::cin >> x3;
+
+	std::cout << "\nВведите числитель дроби 4: ";
+	std::cin >> x4;
+
+
+
+	Fraction f1(x1, x2);
+	Fraction f2(x3, x4);
+
+	Fraction sum_f1_f2 = f1 + f2;
+
+	std::cout << x1 << "/" << x2 << " + " << x3 << "/" << x4 << " = " << sum_f1_f2.numerator_ << '\n';
+
+
+	//42 слайд
+
+
+
+
+	return 0;
+}

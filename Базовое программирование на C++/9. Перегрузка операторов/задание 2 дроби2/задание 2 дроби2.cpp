f3bf10f3961f80f6b968c329cad6f9ef7db1bac6
MrCatVoltSamsung <mrcatvolt@gmail.com>
2023-07-23 01:18:28 +0300

    темы 9. перегрузка задание 2

--- Базовое программирование на C++/9. Перегрузка операторов/задание 2 дроби2/задание 2 дроби2.cpp
+++ Базовое программирование на C++/9. Перегрузка операторов/задание 2 дроби2/задание 2 дроби2.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <Windows.h>
 
 class Fraction
 {
@@ -19,17 +20,121 @@ public:
 
 
 	Fraction operator+(Fraction other) {
-		numerator_ = (numerator_ * other.denominator_) + (denominator_ * other.numerator_);
-		denominator_ = (denominator_ * other.denominator_);
-		
-		return Fraction(numerator_, denominator_);
+		int num=0, den = 0;
+
+		if (denominator_ == other.denominator_) {
+			num = (numerator_ + other.numerator_);
+			den = denominator_;
+		}
+		else {
+			num= (numerator_ * other.denominator_) + (denominator_ * other.numerator_);
+			den = (denominator_ * other.denominator_);
+		}
+		return Fraction(num, den);
+	}
+
+	Fraction operator-(Fraction other) {
+		int num = 0, den = 0;
+		if (denominator_ == other.denominator_) {
+			num = (numerator_ - other.numerator_);
+			den = denominator_;
+		}
+		else {
+			num = (numerator_ * other.denominator_) - (denominator_ * other.numerator_);
+			den = (denominator_ * other.denominator_);
+		}
+		return Fraction(num, den);
+	}
+
+
+	Fraction operator*(Fraction other) {
+		int num = 0, den = 0;
+		num = (numerator_ * other.numerator_);
+		den = (denominator_ * other.denominator_);
+
+		return Fraction(num, den);
+	}
+
+
+	Fraction operator/(Fraction other) {
+		int num = 0, den = 0;
+		num = (numerator_ * other.denominator_);
+		den = (denominator_ * other.numerator_);
+
+		return Fraction(num, den);
+	}
+
+	Fraction& operator++() {
+		numerator_++;
+		denominator_++;
+		return (*this );
+	}
+
+	Fraction operator++(int) {
+		Fraction z = *this;
+		++(*this);
+
+		return z;
+	}
+
+	Fraction& operator--() {
+		numerator_--;
+		denominator_--;
+		return (*this);
+	}
+
+	Fraction operator--(int) {
+		Fraction z = *this;
+		--(*this);
+
+		return z;
+	}
+
+	Fraction& operator-() {
+		numerator_+=5;
+		denominator_+=7;
+		return (*this);
 	}
 
 
 	void get_print() {
+		for (;;) {
+			if (((numerator_ % 2) == 0) && ((denominator_ % 2) == 0)) {
+				numerator_ /= 2;
+				denominator_ /= 2;
+			}
+			else if (((numerator_ % 3) == 0) && ((denominator_ % 3) == 0)) {
+				numerator_ /= 3;
+				denominator_ /= 3;
+			}
+			else if (((numerator_ % 3) == 0) && ((denominator_ % 3) == 0)) {
+				numerator_ /= 3;
+				denominator_ /= 3;
+			}
 
-		std::cout << numerator_ << "/" << denominator_ << "\n";
+			else if (((numerator_ % 5) == 0) && ((denominator_ % 5) == 0)) {
+				numerator_ /= 5;
+				denominator_ /= 5;
+			}
+			else if (((numerator_ % 7) == 0) && ((denominator_ % 7) == 0)) {
+				numerator_ /= 7;
+				denominator_ /= 7;
+			}
+
+			else {
+				std::cout << numerator_ << "/" << denominator_ << "\n";
+				break;
+			}
+		}
 	}
+
+
+
+
+
+
+
+
 
 
 
@@ -46,6 +151,9 @@ public:
 
 int main()
 {
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+
 	int x1 = 0, x2 = 0, x3 = 0, x4 = 0;
 
 	std::cout << "Введите числитель дроби 1: ";
@@ -65,14 +173,59 @@ int main()
 	Fraction f1(x1, x2);
 	Fraction f2(x3, x4);
 
-	Fraction sum_f1_f2 = f1 + f2;
+	Fraction sum_f1_f2 = (f1 + f2);//42 слайд
+	std::cout << x1 << "/" << x2 << " + " << x3 << "/" << x4 << " = ";
+	sum_f1_f2.get_print();
 
-	std::cout << x1 << "/" << x2 << " + " << x3 << "/" << x4 << " = " << sum_f1_f2.numerator_ << '\n';
+	Fraction minus_f1_f2 = (f1 - f2);
+	std::cout << x1 << "/" << x2 << " - " << x3 << "/" << x4 << " = ";
+	minus_f1_f2.get_print();
+
+	Fraction proiz_f1_f2 = (f1 * f2);
+	std::cout << x1 << "/" << x2 << " * " << x3 << "/" << x4 << " = ";
+	proiz_f1_f2.get_print();
+
+	Fraction dell_f1_f2 = (f1 / f2);
+	std::cout << x1 << "/" << x2 << " / " << x3 << "/" << x4 << " = ";
+	dell_f1_f2.get_print();
 
 
-	//42 слайд
 
 
+
+
+	std::cout << "\n\nбазовое f1 = ";
+	f1.get_print();
+	Fraction f00 = (f1++);
+	std::cout << " f00 выполнится и увеличится на 1/1 = ";
+	f00.get_print();
+
+	std::cout << "\nбазовое f1 = ";
+	f1.get_print();
+	Fraction f01 = (++f1);
+	std::cout << " f01 сначала увеличится на 1/1 и выполнится = ";
+	f01.get_print();
+
+	std::cout << "\nбазовое f1 = ";
+	f1.get_print();
+	Fraction f02 = (f1--);
+	std::cout << " f02 выполнится и уменьшится на 1/1 = ";
+	f02.get_print();
+
+	std::cout << "\nбазовое f1 = ";
+	f1.get_print();
+	Fraction f03 = (--f1);
+	std::cout << " f03 сначала уменьшится на 1/1 и выполнится = ";
+	f03.get_print();
+
+	std::cout << "\nбазовое f1 = ";
+	f1.get_print();
+	Fraction f04 = (-f1);
+	std::cout << " f04 увелисится на 5/7 = ";
+	f04.get_print();
+
+	std::cout << "\nбазовое f1 = ";
+	f1.get_print();
 
 
 	return 0;

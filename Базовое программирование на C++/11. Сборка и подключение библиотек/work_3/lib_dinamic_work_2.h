abb72a91f578daf0794f18e4fd862dc485c8099b
MrCatVolt <mrcatvolt@gmail.com>
2023-08-01 17:29:13 +0300

    11_задание_3_подкл через относительный путь и исправление CMake

--- Базовое программирование на C++/11. Сборка и подключение библиотек/work_3/lib_dinamic_work_2.h
+++ Базовое программирование на C++/11. Сборка и подключение библиотек/work_3/lib_dinamic_work_2.h
@@ -2,17 +2,11 @@
 #include <string>
 
 
-#ifdef MYLIBDINAMICWORK2_EXPORTS
-#define MATHPOWERLIBRARY_API __declspec(dllexport)
-#else
-#define MATHPOWERLIBRARY_API __declspec(dllimport)
-#endif
-
 namespace lib_dinamic_work_2
 {
 	class Leaver
 	{
 	public:
-		MATHPOWERLIBRARY_API std::string leave(std::string z);
+	 std::string leave(std::string z);
 	};
 }

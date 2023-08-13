cdf9aa7474978c4bd07c2e18e438cfce202ad670
MrCatVoltSamsung <mrcatvolt@gmail.com>
2023-08-13 06:48:09 +0300

    начало гонок

---
+++ Базовое программирование на C++/99_Итоговая_работа/Race/Race.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+
+// https://github.com/netology-code/cppm-diplom
+
+    // https://github.com/netology-code/cppm-diplom
+
+
+    std::cout << "Hello World!\n";
+
+
+
+
+
+
+    system("pause");
+}

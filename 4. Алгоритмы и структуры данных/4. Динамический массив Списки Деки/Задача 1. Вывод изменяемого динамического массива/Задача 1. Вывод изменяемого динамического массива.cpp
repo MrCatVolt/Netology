d26358e5093b5e2367356700e3633e03e000b71f
MrCatVolt <mrcatvolt@gmail.com>
2023-09-14 07:07:45 +0300

    сортировка

---
+++ 4. Алгоритмы и структуры данных/4. Динамический массив Списки Деки/Задача 1. Вывод изменяемого динамического массива/Задача 1. Вывод изменяемого динамического массива.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+void print_dynamic_array(int* arr, int logical_size, int actual_size) {
+    std::cout << "Динамический массив :";
+    for (int i = 0; i < actual_size; ++i) {
+        std::cout << " " << arr[i];
+
+    }
+
+    for (int i = actual_size; i < logical_size; ++i) {
+        std::cout << " _" ;
+
+
+    }
+
+
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+
+    int a = 0, b = 0, c = 0;
+
+    std::cout << "Введите фактичеcкий размер массива: ";
+    std::cin >> a;
+    std::cout << "Введите логический размер массива: ";
+    std::cin >> b;
+    
+    try { b > a; }
+
+    catch (int a){ std::cout << "Ошибка! Логический размер массива не может превышать фактический!"; }
+
+    int* mas = new int[a];
+
+    while (c < b) {
+        std::cout << "Введите mas"<< "["<<c<<"]: ";
+        std::cin >> mas[c];
+        ++c;
+    }
+
+    print_dynamic_array(mas, a, b);
+
+}
